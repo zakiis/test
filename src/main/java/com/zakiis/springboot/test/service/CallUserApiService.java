858945889aa2779165310b382e9246e2004aa912
Liu Zhenghua <1090179900@qq.com>
2022-04-20 11:45:58 +0800

    damboard test

---
+++ src/main/java/com/zakiis/springboot/test/service/CallUserApiService.java
@@ -0,0 +1,19 @@
+package com.zakiis.springboot.test.service;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.zakiis.springboot.test.model.User;
+
+@FeignClient(url = "http://fake-address", name="FAKE-USER-SERVICE")
+public interface CallUserApiService {
+
+	@GetMapping("/api/v1/user/{userId}")
+	User getUserById(@RequestParam("userId")Long userId);
+	
+	@GetMapping("/api/v1/user/selectAll")
+	List<User> selectAll();
+}
